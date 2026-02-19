3691786142c7686b2db9ac637b14202237f43e4f
Mehak2401 <mehak24012008@gmail.com>
2026-02-19 10:47:50 +0530

    UC1: Completed simple OOPS console print

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp - Use Case 1
+ * Foundational step to print "OOPS" to the console.
+ * @author DELL
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        // Core Logic: Printing literal text
+        printSimpleOOPS();
+    }
+
+    public static void printSimpleOOPS() {
+        System.out.println("OOPS");
+    }
+}
