c535aa12ff59f3df8fc8199a15f2991e456888b4
Mehak2401 <mehak24012008@gmail.com>
2026-02-22 14:02:42 +0530

    UC2: Created stylized OOPS banner

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerApp - Use Case 2
+ * Goal: Stylized ASCII art display of "OOPS"
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("  *** *** **** **** ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * **** **** ");
+        System.out.println(" * * * * * *");
+        System.out.println("  *** *** * **** ");
+    }
+}
